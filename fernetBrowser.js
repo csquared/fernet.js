507ec14f43a713fa618a95afe2de1f48dba1c1ad
Chris Continanza <christopher.continanza@gmail.com>
2014-10-08 13:41:07 -0700

    small fixes

--- fernetBrowser.js
+++ fernetBrowser.js
@@ -146,6 +146,9 @@ var f = require('../fernet');
 
 var Secret = function(secret64){
   var secret = f.decode64toHex(secret64);
+  if (secret.length !== f.hexBits(256)) {
+      throw new Error('Secret must be 32 url-safe base64-encoded bytes.');
+  }
   this.signingKeyHex    = secret.slice(0,f.hexBits(128));
   this.signingKey       = f.Hex.parse(this.signingKeyHex);
   this.encryptionKeyHex = secret.slice(f.hexBits(128));
@@ -163,6 +166,7 @@ module = module.exports = function(parent){
     opts = opts || {};
     this.secret     = opts.secret || parent.secret;
     this.ttl        = opts.ttl    || parent.ttl;
+    if(opts.ttl === 0) this.ttl = 0;
     this.message    = opts.message;
     this.cipherText = opts.cipherText;
     this.token      = opts.token;
