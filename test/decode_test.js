f5ecdda57b3c35cd88c0c61fef2bf988f386acc6
stephinson <40458699+stephinson@users.noreply.github.com>
2025-04-14 20:33:30 +0530

    test: refactor test suite from suite/test to describe/it syntax

--- test/decode_test.js
+++ test/decode_test.js
@@ -1,7 +1,7 @@
-//for browser compatibility
+// for browser compatibility
 if (!chai) var chai = require('chai');
 if (!sinon) var sinon = require("sinon");
-if (!sinonChai) var sinonChai = require("sinon-chai");
+if (!sinonChai) var sinonChai = require("sinon-chai").default;
 if (!fernet) var fernet = require('../fernet');
 
 var assert = chai.assert
@@ -22,11 +22,11 @@ var unacceptableClockSkewTestData = {
 }
 
 
-suite('fernet.Token.prototype.decode', function () {
+describe('fernet.Token.prototype.decode', function () {
   var _fernet = new fernet({ ttl: 0 })
   var secret = new fernet.Secret(testData.secret);
 
-  test("decode()", function () {
+  it("decode()", function () {
     var token = new _fernet.Token({
       secret: secret,
       token: testData.token
@@ -36,20 +36,20 @@ suite('fernet.Token.prototype.decode', function () {
     assert.equal("hello", token.toString())
   })
 
-  test("decode(token)", function () {
+  it("decode(token)", function () {
     var token = new _fernet.Token({ secret: secret })
     assert.equal("hello", token.decode(testData.token))
     assert.equal("hello", token.toString())
   })
 
-  test("decode(token) with top-level secret", function () {
+  it("decode(token) with top-level secret", function () {
     var f = new fernet({ secret: testData.secret, ttl: 0 })
     var token = new f.Token()
     assert.equal("hello", token.decode(testData.token))
     assert.equal("hello", token.toString())
   })
 
-  test('recovers version', function () {
+  it('recovers version', function () {
     var token = new _fernet.Token({
       secret: secret,
       token: testData.token,
@@ -60,7 +60,7 @@ suite('fernet.Token.prototype.decode', function () {
     assert.equal(token.version, 128);
   })
 
-  test('recovers time', function () {
+  it('recovers time', function () {
     var token = new _fernet.Token({
       secret: secret,
       token: testData.token
@@ -70,7 +70,7 @@ suite('fernet.Token.prototype.decode', function () {
     assert.equal(token.time.toUTCString(), now.toUTCString());
   })
 
-  test('recovers iv', function () {
+  it('recovers iv', function () {
     var token = new _fernet.Token({
       secret: secret,
       token: testData.token
@@ -80,7 +80,7 @@ suite('fernet.Token.prototype.decode', function () {
     assert.equal(token.ivHex, ivHex);
   })
 
-  test('recovers hmac', function () {
+  it('recovers hmac', function () {
     var token = new _fernet.Token({
       secret: secret,
       token: testData.token
@@ -90,7 +90,7 @@ suite('fernet.Token.prototype.decode', function () {
     assert.equal(token.hmacHex, computedHmac.toString(fernet.Hex));
   })
 
-  test('inherits parent TTL', function () {
+  it('inherits parent TTL', function () {
     var f = new fernet({ ttl: 1 });
     var token = new f.Token({
       secret: secret,
@@ -102,7 +102,7 @@ suite('fernet.Token.prototype.decode', function () {
     }, Error, 'Invalid Token: TTL');
   })
 
-  test('raises new Error("Invalid Token: TTL") on invalid ttl', function () {
+  it('raises new Error("Invalid Token: TTL") on invalid ttl', function () {
     var token = new fernet.Token({
       secret: secret,
       token: testData.token,
@@ -114,7 +114,7 @@ suite('fernet.Token.prototype.decode', function () {
     }, Error, 'Invalid Token: TTL');
   })
 
-  test('raises new Error("Invalid version") on wrong version byte', function () {
+  it('raises new Error("Invalid version") on wrong version byte', function () {
     var tokenHex = fernet.decode64toHex(testData.token);
     var versionOffset = fernet.hexBits(8);
     var dirtyToken = '01' + tokenHex.slice(versionOffset);
@@ -127,7 +127,7 @@ suite('fernet.Token.prototype.decode', function () {
     }, Error, 'Invalid version');
   })
 
-  test('raises new Error("Invalid Token: HMAC") on wrong Hmac', function () {
+  it('raises new Error("Invalid Token: HMAC") on wrong Hmac', function () {
     var s = testData.token;
     var i = s.length - 5;
     var mutation = String.fromCharCode(s.charCodeAt(i) + 1);
@@ -142,7 +142,7 @@ suite('fernet.Token.prototype.decode', function () {
     }, Error, 'Invalid Token: HMAC');
   })
 
-  test('raises new Error("far-future timestamp") on unacceptable clock skew', function () {
+  it('raises new Error("far-future timestamp") on unacceptable clock skew', function () {
     var token = new fernet.Token({
       secret: new fernet.Secret(unacceptableClockSkewTestData.secret),
       token: unacceptableClockSkewTestData.token,
--- test/secret_test.js
+++ test/secret_test.js
@@ -4,34 +4,34 @@ if (!fernet) var fernet = require('../fernet');
 var assert = chai.assert;
 
 
-suite('fernet.Secret', function () {
+describe('fernet.Secret', function () {
 
   var secret64 = "cw_0x689RpI-jtRR7oE8h_eQsKImvJapLeSbXpwF4e4=";
   var signingKeyHex = '730ff4c7af3d46923e8ed451ee813c87';
   var encryptionKeyHex = 'f790b0a226bc96a92de49b5e9c05e1ee';
   var secret = new fernet.Secret(secret64);
 
-  test('secret.signingKeyHex', function () {
+  it('secret.signingKeyHex', function () {
     assert.equal(secret.signingKeyHex, signingKeyHex);
-  })
+  });
 
-  test('secret.signingKey', function () {
+  it('secret.signingKey', function () {
     assert.deepEqual(secret.signingKey, fernet.Hex.parse(signingKeyHex));
-  })
+  });
 
-  test('secret.encryptionKeyHex', function () {
+  it('secret.encryptionKeyHex', function () {
     assert.equal(secret.encryptionKeyHex, encryptionKeyHex);
-  })
+  });
 
-  test('secret.encryptionKey', function () {
+  it('secret.encryptionKey', function () {
     assert.deepEqual(secret.encryptionKey, fernet.Hex.parse(encryptionKeyHex));
-  })
+  });
 
-  test('raises "new Error(\'Secret must be 32 url-safe base64-encoded bytes.\')" on wrong secret', function () {
+  it('raises "new Error(\'Secret must be 32 url-safe base64-encoded bytes.\')" on wrong secret', function () {
 
     assert.throws(function () {
       new fernet.Secret('not a good secret');
     }, Error, 'Secret must be 32 url-safe base64-encoded bytes.');
   })
 
-})
+});
